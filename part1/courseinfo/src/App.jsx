46572e66f2a63a7362b42681f59426ca1dd1a012
ketamethane <cheonghsien@gmail.com>
2024-02-28 20:52:02 +0800

    Add courseinfo step3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -34,12 +34,18 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
 // what is the difference between having 3 content and 1 content?
 // the use of fragments and less div elements?
@@ -47,8 +53,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content number={[part1, part2, part3]} numEx={[exercises1, exercises2, exercises3]} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content number={[part1.name, part2.name, part3.name]} numEx={[part1.exercises, part2.exercises, part3.exercises]} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
