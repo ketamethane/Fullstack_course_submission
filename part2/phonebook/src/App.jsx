1ba1ac0d3cb3cb130aa3f191841d021976d0782c
ketamethane <cheonghsien@gmail.com>
2024-03-06 23:27:18 +0800

    Add phonebook step1

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,24 +1,57 @@
 import { useState } from 'react'
 
+const Person = ({person}) => {
+  return (
+    <>
+    <p>{person}</p>
+    </>
+  )
+}
+
+
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
 
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    console.log('Add Person', event.target)
+    // may need to add id to each person in the future
+    const newPerson = {
+      name: newName
+    }
+
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+  
+  const handlePersonChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName}
+          onChange={handlePersonChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person =>
+        <Person key={person.name} person={person.name}/>
+        )}
     </div>
   )
 }
