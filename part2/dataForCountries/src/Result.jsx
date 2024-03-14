ed5d0cc1e308c0fefa30650ed3a35d433ae853e6
ketamethane <cheonghsien@gmail.com>
2024-03-14 21:17:14 +0800

    Add dataforcountries step2

--- part2/dataForCountries/src/Result.jsx
+++ part2/dataForCountries/src/Result.jsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
+
 const Country = ({name, capital, area, languages, flag}) => {
     console.log('langs:', languages)
     console.log('langs values:', Object.values(languages))
@@ -20,15 +24,42 @@ const Country = ({name, capital, area, languages, flag}) => {
     )
 }
 
-const CountryInList = ({name}) => {
-    return (
-            {name}
-    )
-}
 
 const Result = ({data, input}) => {
     const filteredResult = data.filter(cty => cty.name.common.toLowerCase().includes(input))
     console.log('result: filtered countries', filteredResult)
+
+    const [show, setShow] = useState(false)
+    const [country, setCountry] = useState(false)
+    useEffect(() => {
+        setShow(false)
+
+    }, [input])
+
+    const viewCountry = (name) => {
+        const lowerCaseName = name.toLowerCase()
+        console.log('lCName', lowerCaseName)
+        axios
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${lowerCaseName}`)
+        .then(response => {
+          console.log(response.data)
+          setCountry(response.data)
+          setShow(true)
+        })
+    }
+
+    if (show===true) {
+
+        return(
+            <div>
+                <Country key={country.name} name={country.name.common} 
+                capital={country.capital[0]} area={country.area} 
+                languages={country.languages} flag={country.flags.png}
+                /> 
+            </div>
+        )
+        }
+
 
     if (filteredResult.length > 10 && input!=='') {
         console.log('more than 10 countries')
@@ -39,16 +70,24 @@ const Result = ({data, input}) => {
 
     if ((filteredResult.length <= 10 && filteredResult.length > 1) && input!=='') {
         console.log('less than 10 countries')
+        const list = []
+        filteredResult.forEach(cty => list.push(cty.name.common))
+        console.log('list', list)
         return (
             <div>
-            {filteredResult.map(cty => 
-                <CountryInList key={cty.name.common} name={cty.name.common}></CountryInList>
-            )}
+            {list.map((country, index) => (
+        <div key={index}>
+          <span>{country}</span>
+          <button onClick={() => viewCountry(country)}>show</button>
+          
+        </div>
+        ))}
             </div>
         )
     }
 
-    if (filteredResult.length===1 && input!=='') {
+    if (filteredResult.length===1 && input!=='' || show===true) {
+        
         console.log('found the country!',filteredResult[0])
         return(
             <div>
