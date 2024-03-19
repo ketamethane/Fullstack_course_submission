998436e6c3fdf89c8dd43a64a4e287bdc237fae2
ketamethane <cheonghsien@gmail.com>
2024-03-19 23:23:39 +0800

    Add part3 phonebook step1

---
+++ part3/phonebook/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+// only one send() statement per get
+// figure out how to get number of entries and show time
+app.get('/', (request, response) => {
+
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/', (request, response) => {
+    
+    response.send('<h1>Hello World!</h1>')
+  })
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
