6f33c9d69ee09ce01273fa11e6af61fdd1c5c8c8
ketamethane <cheonghsien@gmail.com>
2024-03-20 22:32:49 +0800

    Add phonebook step5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+// middleware!
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -89,6 +92,39 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+})
+
+const generateId = (max) => {
+  return Math.floor(Math.random() * max);
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  console.log('body', body)
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(100000000),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
