353e7ec88fc15ee1d4fe5ae84a95c263b9e44fc6
ketamethane <cheonghsien@gmail.com>
2024-03-20 22:07:01 +0800

    Add part 3 phonebook step 2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -25,15 +25,48 @@ let persons = [
 ]
 
 // only one send() statement per get
-// figure out how to get number of entries and show time
 app.get('/', (request, response) => {
 
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/', (request, response) => {
-    
-    response.send('<h1>Hello World!</h1>')
+// figure out how to get number of entries and show time
+// fill in number and time
+app.get('/info', (request, response) => {
+    // Create a new Date object
+  var now = new Date();
+
+  // Arrays for day and month names
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+  // Extract date and time components
+  var dayOfWeek = days[now.getDay()];
+  var month = months[now.getMonth()];
+  var day = now.getDate();
+  var year = now.getFullYear();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+  var timezoneOffset = now.getTimezoneOffset();
+
+  // Format time to 12-hour format and determine AM/PM
+  var ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  // Format timezone offset
+  var timezone = timezoneOffset < 0 ? 'GMT+' + (-timezoneOffset / 60) : 'GMT-' + (timezoneOffset / 60);
+
+  // Combine all components into a single string
+  var formattedDateTime = dayOfWeek + ', ' + month + ' ' + day + ', ' + year + ', ' + hours + ':' + minutes + ':' + seconds + ' ' + ampm + ', ' + timezone;
+
+  console.log(formattedDateTime);
+
+    const num = persons.length;
+    response.send(`<p>Phonebook has info for ${num} people</p><p>${formattedDateTime}</p>`)
   })
 
 app.get('/api/persons', (request, response) => {
