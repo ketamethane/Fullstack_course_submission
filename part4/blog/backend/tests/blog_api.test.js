6bc0a67e14d9c5ae401587281bbedfa3ab8480a8
ketamethane <cheonghsien@gmail.com>
2024-04-19 01:19:36 +0800

    Add ex 4.23 and fix test bugs

--- part4/blog/backend/tests/blog_api.test.js
+++ part4/blog/backend/tests/blog_api.test.js
@@ -6,6 +6,7 @@ const helper = require('./test_helper')
 const assert = require('node:assert')
 const supertest = require('supertest')
 const app = require('../app')
+const jwt = require('jsonwebtoken')
 
 const api = supertest(app)
 
@@ -22,7 +23,7 @@ const api = supertest(app)
 
 
 describe('when there is initially some blogs saved', () => {
-  let testUserId
+  // let testUserId
 
   beforeEach(async () => {
     await Blog.deleteMany({})
@@ -57,171 +58,373 @@ describe('when there is initially some blogs saved', () => {
 
     assert.strictEqual(response.body.length, helper.initialBlogs.length)
   })
+})
 
+describe('validity testing', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
 
-  describe('validity testing', () => {
-    beforeEach(async () => {
-      await Blog.deleteMany({})
-      await Blog.insertMany(helper.initialBlogs)
-    })
-
-    test('unique identifier property of blog posts is named id', async () => {
-      const response = await api.get('/api/blogs')
-
-      response.body.forEach((blog) => {
-
-        assert.strictEqual(Object.prototype.hasOwnProperty.call(blog, 'id'), true)
-      })
-    })
-
-
-
-    test.only('a valid blog can be added ', async () => {
-
-      const testUser = new User({
-        username: 'testuser',
-        name: 'Test User',
-      // Add other necessary fields
-      })
-      const savedUser = await testUser.save()
-      testUserId = savedUser._id // Save the user ID for use in tests
-
-      const newBlog = {
-        title: 'Vysions',
-        author: 'vylt',
-        url: 'www.vylt.com',
-        likes: 1000,
-        userId: testUserId
-      }
-
-
-      await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-
-      const response = await api.get('/api/blogs')
-
-      const titles = response.body.map(r => r.title)
-
-      assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
-
-      assert(titles.includes('Vysions'))
-    })
-
-    test('blog without likes defaults to zero', async () => {
-      const newBlog = {
-        title: 'violet',
-        author: 'vylt',
-        url: 'www.vylt.com'
-      }
-
-      await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-
-      const response = await api.get('/api/blogs')
-
-      const createdBlog = response.body.find(blog => blog.title === newBlog.title)
-
-      // Verify the blog has zero likes
-      assert.strictEqual(createdBlog.likes, 0)
-    })
+    const existingUser = await User.findOne({ username: 'testuser' })
+    if (existingUser) {
+      await User.findByIdAndDelete(existingUser.id)
+    }
   })
 
-  describe('Error 400 tests', () => {
-    beforeEach(async () => {
-      await Blog.deleteMany({})
-      await Blog.insertMany(helper.initialBlogs)
-    })
-    test('missing title property results in 400 Bad Request', async () => {
-      const newBlog = {
-        author: 'Test Author',
-        url: 'http://test.com',
-        // Intentionally omit the 'title' property
-      }
+  test('unique identifier property of blog posts is named id', async () => {
+    const response = await api.get('/api/blogs')
 
-      await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
-    })
+    response.body.forEach((blog) => {
 
-
-    // Test for missing 'url' property
-    test('missing url property results in 400 Bad Request', async () => {
-      const newBlog = {
-        title: 'Test Blog',
-        author: 'Test Author',
-        // Intentionally omit the 'url' property
-      }
-
-      await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
+      assert.strictEqual(Object.prototype.hasOwnProperty.call(blog, 'id'), true)
     })
   })
 
 
-  describe('deletion of a blog', () => {
-    test('succeeds with status code 204 if id is valid', async () => {
-      const blogsAtStart = await helper.blogsInDb()
-      const blogToDelete = blogsAtStart[0]
 
-      await api
-        .delete(`/api/blogs/${blogToDelete.id}`)
-        .expect(204)
-
-      const blogsAtEnd = await helper.blogsInDb()
-
-      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
-
-      const titles = blogsAtEnd.map(r => r.title)
-      assert(!titles.includes(blogToDelete.title))
+  test('a valid blog can be added ', async () => {
+    const testUser = new User({
+      username: 'testuser',
+      name: 'Test User',
+      password: 'hello'
     })
+    const savedUser = await testUser.save()
+    const testUserId = savedUser._id // Save the user ID for use in tests
 
-    test('fails with status code 404 if id does not exist', async () => {
-      const nonExistingId = await helper.nonExistingId()
+    const token = jwt.sign({ id: savedUser._id }, process.env.SECRET)
 
-      await api
-        .delete(`/api/notes/${nonExistingId}`)
-        .expect(404)
-    })
+    const newBlog = {
+      title: 'Vysions',
+      author: 'vylt',
+      url: 'www.vylt.com',
+      likes: 1000,
+      userId: testUserId
+    }
+
+    // Include the token in the headers of the POST request
+    await api
+      .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`) // Set the Authorization header
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    const titles = response.body.map(r => r.title)
+
+    assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
+
+    assert(titles.includes('Vysions'))
+
   })
 
-  describe('PUT /api/blogs/:id', () => {
-    test('should update the number of likes for a blog post', async () => {
-      const blogsAtStart = await helper.blogsInDb()
-      const blogToUpdate = blogsAtStart[0]
 
-      // Send a PUT request to update the number of likes
-      const updatedBlog = await api
-        .put(`/api/blogs/${blogToUpdate.id}`)
-        .send({ likes: 10 })
-        .expect(200)
 
-      assert.strictEqual(updatedBlog.body.likes, 10)
+
+  test('blog without likes defaults to zero', async () => {
+
+    const testUser = new User({
+      username: 'testuser',
+      name: 'Test User',
+      password: 'hello'
     })
+    const savedUser = await testUser.save()
+    const testUserId = savedUser._id // Save the user ID for use in tests
 
-    test('fails when likes property is missing', async () => {
-      const blogsAtStart = await helper.blogsInDb()
-      const blogToUpdate = blogsAtStart[0]
+    const token = jwt.sign({ id: savedUser._id }, process.env.SECRET)
 
-      const response = await api
-        .put(`/api/blogs/${blogToUpdate.id}`)
-        .send({})
-        .expect(400)
+    const newBlog = {
+      title: 'Vysions',
+      author: 'vylt',
+      url: 'www.vylt.com',
+      userId: testUserId
+    }
 
-      assert.strictEqual(response.body.error, 'likes value is missing')
-    })
+    // Include the token in the headers of the POST request
+    await api
+      .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`) // Set the Authorization header
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    const titles = response.body.map(r => r.title)
+
+    assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
+
+    assert(titles.includes('Vysions'))
+  })
+
+
+  test('should fail with status code 401 Unauthorized', async () => {
+    const newBlog = {
+      title: 'Test Blog',
+      author: 'Test Author',
+      url: 'http://test.com',
+      likes: 10
+    }
+
+    // Make a POST request to create a new blog without setting the Authorization header
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(401) // Expect a 401 Unauthorized status code
   })
 })
 
 
+describe('Error 400 tests', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+
+    const existingUser = await User.findOne({ username: 'testuser' })
+    if (existingUser) {
+      await User.findByIdAndDelete(existingUser.id)
+    }
+  })
+  test('missing title property results in 400 Bad Request', async () => {
+    const testUser = new User({
+      username: 'testuser',
+      name: 'Test User',
+      password: 'hello'
+    })
+    const savedUser = await testUser.save()
+    const testUserId = savedUser._id // Save the user ID for use in tests
+
+    const token = jwt.sign({ id: savedUser._id }, process.env.SECRET)
+
+    const newBlog = {
+      author: 'Test Author',
+      url: 'http://test.com',
+      userId: testUserId
+      // Intentionally omit the 'title' property
+    }
+
+    await api
+      .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`)
+      .send(newBlog)
+      .expect(400)
+  })
+
+
+  // Test for missing 'url' property
+  test('missing url property results in 400 Bad Request', async () => {
+    const testUser = new User({
+      username: 'testuser',
+      name: 'Test User',
+      password: 'hello'
+    })
+    const savedUser = await testUser.save()
+    const testUserId = savedUser._id // Save the user ID for use in tests
+
+    const token = jwt.sign({ id: savedUser._id }, process.env.SECRET)
+    const newBlog = {
+      title: 'Test Blog',
+      author: 'Test Author',
+      userId: testUserId
+      // Intentionally omit the 'url' property
+    }
+
+    await api
+      .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`)
+      .send(newBlog)
+      .expect(400)
+  })
+})
+
+
+describe('deletion of a blog', () => {
+  // do i add then delete, or delete before each and add within
+  let testUserId
+  let testBlogId
+  let token
+
+  beforeEach(async () => {
+
+    const existingUser = await User.findOne({ username: 'testuser' })
+    if (existingUser) {
+      const savedUser = await existingUser.save()
+      testUserId = savedUser._id // Save the user ID for use in tests
+
+      // Generate a token for the test user
+      token = jwt.sign({ id: savedUser._id }, process.env.SECRET)
+
+      // Create a test blog tied to the test user
+      const testBlog = new Blog({
+        title: 'Test Blog',
+        author: 'Test Author',
+        url: 'http://test.com',
+        likes: 10,
+        user: testUserId
+      })
+      const savedBlog = await testBlog.save()
+      testBlogId = savedBlog._id // Save the blog ID for use in tests
+    } else {
+      // Create a test user
+      const testUser = new User({
+        username: 'testuser',
+        name: 'Test User',
+        // Add other necessary fields
+      })
+      const savedUser = await testUser.save()
+      testUserId = savedUser._id // Save the user ID for use in tests
+
+      // Generate a token for the test user
+      token = jwt.sign({ id: savedUser._id }, process.env.SECRET)
+
+      // Create a test blog tied to the test user
+      const testBlog = new Blog({
+        title: 'Test Blog',
+        author: 'Test Author',
+        url: 'http://test.com',
+        likes: 10,
+        user: testUserId
+      })
+      const savedBlog = await testBlog.save()
+      testBlogId = savedBlog._id // Save the blog ID for use in tests
+    }
+    // // Create a test user
+    // const testUser = new User({
+    //   username: 'testuser',
+    //   name: 'Test User',
+    //   // Add other necessary fields
+    // })
+    // const savedUser = await testUser.save()
+    // testUserId = savedUser._id // Save the user ID for use in tests
+
+    // // Generate a token for the test user
+    // token = jwt.sign({ id: savedUser._id }, process.env.SECRET)
+
+    // // Create a test blog tied to the test user
+    // const testBlog = new Blog({
+    //   title: 'Test Blog',
+    //   author: 'Test Author',
+    //   url: 'http://test.com',
+    //   likes: 10,
+    //   user: testUserId
+    // })
+    // const savedBlog = await testBlog.save()
+    // testBlogId = savedBlog._id // Save the blog ID for use in tests
+  })
+
+
+  test('succeeds with status code 204 if id is valid', async () => {
+    // const blogsAtStart = await helper.blogsInDb()
+    // const blogToDelete = blogsAtStart[0]
+
+
+    await api
+      .delete(`/api/blogs/${testBlogId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(204)
+
+    // const blogsAtEnd = await helper.blogsInDb()
+
+    // // no change to db size since I added a test blog and then deleted it.
+    // assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+
+    // const titles = blogsAtEnd.map(r => r.title)
+    // assert(!titles.includes(blogToDelete.title))
+  })
+
+  test('fails with status code 404 if id does not exist', async () => {
+    const nonExistingId = await helper.nonExistingId()
+
+    await api
+      .delete(`/api/notes/${nonExistingId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(404)
+  })
+})
+
+describe('PUT /api/blogs/:id', () => {
+
+  let testUserId
+  let testBlogId
+  let token
+
+  beforeEach(async () => {
+
+    const existingUser = await User.findOne({ username: 'testuser' })
+    if (existingUser) {
+      const savedUser = await existingUser.save()
+      testUserId = savedUser._id // Save the user ID for use in tests
+
+      // Generate a token for the test user
+      token = jwt.sign({ id: savedUser._id }, process.env.SECRET)
+
+      // Create a test blog tied to the test user
+      const testBlog = new Blog({
+        title: 'Test Blog',
+        author: 'Test Author',
+        url: 'http://test.com',
+        likes: 10,
+        user: testUserId
+      })
+      const savedBlog = await testBlog.save()
+      testBlogId = savedBlog._id // Save the blog ID for use in tests
+    } else {
+      // Create a test user
+      const testUser = new User({
+        username: 'testuser',
+        name: 'Test User',
+        // Add other necessary fields
+      })
+      const savedUser = await testUser.save()
+      testUserId = savedUser._id // Save the user ID for use in tests
+
+      // Generate a token for the test user
+      token = jwt.sign({ id: savedUser._id }, process.env.SECRET)
+
+      // Create a test blog tied to the test user
+      const testBlog = new Blog({
+        title: 'Test Blog',
+        author: 'Test Author',
+        url: 'http://test.com',
+        likes: 10,
+        user: testUserId
+      })
+      const savedBlog = await testBlog.save()
+      testBlogId = savedBlog._id // Save the blog ID for use in tests
+    }
+  })
+  test('should update the number of likes for a blog post', async () => {
+    // const blogsAtStart = await helper.blogsInDb()
+    // const blogToUpdate = blogsAtStart[0]
+
+    // Send a PUT request to update the number of likes
+    const updatedBlog = await api
+      .put(`/api/blogs/${testBlogId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({ likes: 10 })
+      .expect(200)
+
+    assert.strictEqual(updatedBlog.body.likes, 10)
+  })
+
+  test('fails when likes property is missing', async () => {
+    // const blogsAtStart = await helper.blogsInDb()
+    // const blogToUpdate = blogsAtStart[0]
+
+    const response = await api
+      .put(`/api/blogs/${testBlogId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({})
+      .expect(400)
+
+    assert.strictEqual(response.body.error, 'likes value is missing')
+  })
+})
+
+
+
 after(async () => {
+
   await mongoose.connection.close()
 })
