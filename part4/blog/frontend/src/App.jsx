09cc5b1b8c287363f6dd88b016a2daf75d624890
ketamethane <cheonghsien@gmail.com>
2024-05-08 20:46:38 +0800

    Add Ex 5.5

--- part4/blog/frontend/src/App.jsx
+++ part4/blog/frontend/src/App.jsx
@@ -4,6 +4,8 @@ import blogService from './services/blogs'
 import loginService from './services/login'
 import Notification from './components/Notification'
 import BlogForm from './components/BlogForm'
+import LoginForm from './components/Login'
+import Togglable from './components/Togglable'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -17,6 +19,7 @@ const App = () => {
   const [notifAuthor, setNotifAuthor] = useState('')
   const [isError, setIsError] = useState(false)
   const [user, setUser] = useState(null)
+  // const [loginVisible, setLoginVisible] = useState(false)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -61,30 +64,48 @@ const App = () => {
       }, 5000)
     }
   }
+  const loginForm = () => {
+    // const hideWhenVisible = { display: loginVisible ? 'none' : '' }
+    // const showWhenVisible = { display: loginVisible ? '' : 'none' }
 
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
+    return (
       <div>
-        username
-          <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
+        <Togglable buttonLabel='login'>
+          <LoginForm
+            username={username}
+            password={password}
+            handleUsernameChange={({ target }) => setUsername(target.value)}
+            handlePasswordChange={({ target }) => setPassword(target.value)}
+            handleSubmit={handleLogin}
+          />
+        </Togglable>
       </div>
-      <div>
-        password
-          <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>      
-  )
+    )
+  }
+
+  // const loginForm = () => (
+  //   <form onSubmit={handleLogin}>
+  //     <div>
+  //       username
+  //         <input
+  //         type="text"
+  //         value={username}
+  //         name="Username"
+  //         onChange={({ target }) => setUsername(target.value)}
+  //       />
+  //     </div>
+  //     <div>
+  //       password
+  //         <input
+  //         type="password"
+  //         value={password}
+  //         name="Password"
+  //         onChange={({ target }) => setPassword(target.value)}
+  //       />
+  //     </div>
+  //     <button type="submit">login</button>
+  //   </form>      
+  // )
 
   // const noteForm = () => (
   //   <div></div>
@@ -246,10 +267,13 @@ const App = () => {
       {user === null ?
       loginForm() :
       <div>
-        <p>{user.name} logged-in</p>
+        <p>{user.name} logged-in
         <button onClick={logout}>Logout</button>
-        <BlogForm onSubmit={addBlog} title={newTitle} titleHandle={handleTitleChange}
-      author={newAuthor} authorHandle={handleAuthorChange} url={newUrl} urlHandle={handleUrlChange} />
+        </p>
+        <Togglable buttonLabel="new note">
+          <BlogForm onSubmit={addBlog} title={newTitle} titleHandle={handleTitleChange}
+        author={newAuthor} authorHandle={handleAuthorChange} url={newUrl} urlHandle={handleUrlChange} />
+        </Togglable>
       </div>
     }
 
@@ -262,204 +286,3 @@ const App = () => {
 }
 
 export default App
-
-
-// import { useState, useEffect } from 'react'
-// import Filter from './components/Filter'
-// import PersonForm from './components/PersonForm'
-// import Persons from './components/Persons'
-// import personService from './services/persons'
-// import Notification from './components/Notification'
-
-
-
-// code needs a backend. need to config backend. backend code somewhat done
-// need to connect frontend to end via the services
-// use provided template as base
-// I need to ensure there is a state of being logged in with token.
-// use local storage
-
-// ensure backend ready
-// config basic FE (show blogs)
-// login
-
-// does not verify if number field contains numbers only
-
-// const App = () => {
-//   const [persons, setPersons] = useState([])
-//   const [newName, setNewName] = useState('')
-//   const [newNum, setNewNum] = useState('')
-//   const [filter, setFilter] = useState('')
-//   const [message, setMessage] = useState(null)
-//   const [isError, setIsError] = useState(false)
-
-//   useEffect(() => {
-//     personService.
-//       getAll().
-//       then(initialPersons => {
-//           console.log('got all')
-//           setPersons(initialPersons)
-//       })
-//   }, [])
-
-//   // console.log('render', persons.length, 'persons')
-
-//   // adding person works
-
-//   const addPerson = (event) => {
-//     event.preventDefault()
-//     console.log('Add Person', event.target)
-
-//     const newPerson = {
-//       name: newName,
-//       number: newNum,
-//       // id: (persons.length + 1).toString()
-//     }
-//     // console.log('new person id after', newPerson.id)
-
-//     const isPersonAlreadyAdded = persons.some(person => person.name === newPerson.name)
-
-//     if (isPersonAlreadyAdded) {
-//       if (window.confirm(`${newPerson.name} is already added to phonebook, replace the old number with a new one?`)) {
-        
-//         // make a shallow copy of the person to update with that person's id, then update accordingly
-//         const addedPerson = persons.find(person => person.name === newPerson.name)
-//         const changedPerson = { ...addedPerson, number: newNum}
-//         console.log('changed person:', changedPerson)
-//         console.log('changed person id: ', changedPerson.id)
-//         personService.update(changedPerson.id, changedPerson)
-//         .then(response => {
-//           console.log('added person:',response)
-//           setPersons(persons.map(p => p.id !== changedPerson.id ? p : response))
-//           setNewName('')
-//           setNewNum('')
-//           setMessage(
-//             `Changed ${newPerson.name}'s number to ${newPerson.number}`
-//           )
-//           setTimeout(() => {
-//             setMessage(null)
-//           }, 5000)
-//         }
-//         )
-//         .catch(error => {
-//           console.log('error:', error)
-//           setIsError(true)
-//           setMessage(
-//             `Information of ${newPerson.name} has already been removed from server`
-//           )
-//           setTimeout(() => {
-//             setMessage(null)
-//             setIsError(false)
-//           }, 5000)
-//         }
-//         )
-//         return
-
-//       }
-//       // console.log(`${newPerson.name} is already added to phonebook, replace the old number with a new one?`)
-//       // alert(`${newPerson.name} is already added to phonebook`)
-//       // return
-//     }
-
-//     // if (newNum === '') {
-//     //   alert(`Please enter a number`)
-//     //   return
-//     // }
-
-//     const isNumberAlreadyAdded = persons.some(person => person.number === newPerson.number)
-
-
-//     if (isNumberAlreadyAdded) {
-//       console.log(`${newPerson.number} is already added to phonebook`)
-//       alert(`${newPerson.number} is already added to phonebook`)
-//       return
-//     }
-
-//     // const newPersons = persons.concat(newPerson)
-
-//     personService.create(newPerson)
-//       .then(response => {
-//         console.log(response, 'response')
-//         console.log('added new person')
-//         setPersons(persons.concat(response))
-//         setNewName('')
-//         setNewNum('')
-//         setMessage(
-//           `Added ${newPerson.name}`
-//         )
-//         setTimeout(() => {
-//           setMessage(null)
-//         }, 5000)
-//       })
-//       .catch(error => {
-//         setIsError(true)
-//           setMessage(
-//             error.response.data.error
-//           )
-//           setTimeout(() => {
-//             setIsError(false)
-//             setMessage(null)
-//           }, 5000)
-//         console.log(error.response.data.error)
-//       })
-//   }
-
-//   const deletePerson = (person) => {
-//     console.log('person in del', person)
-//     console.log('person id:', person.id)
-//     if (window.confirm(`Delete ${person.name} ?`)) {
-//       console.log('person id:', person.id)
-//     personService.deletePerson(person.id)
-//       .then(response => {
-//         console.log(response, 'delete')
-//         setPersons(persons.filter(p => p.id !== person.id))
-//       })
-//       .catch(error => {
-//         console.log('error:', error)
-//         setIsError(true)
-//         setMessage(
-//           `Information of ${person.name} has already been removed from server`
-//         )
-//         setTimeout(() => {
-//           setMessage(null)
-//           setIsError(false)
-//         }, 5000)
-//       }
-//       )
-//     }
-//   }
-
-  
-//   const handlePersonChange = (event) => {
-//     console.log(event.target.value)
-//     setNewName(event.target.value)
-//   }
-//   const handleNumberChange = (event) => {
-//     console.log(event.target.value)
-//     setNewNum(event.target.value)
-//   }
-
-//   const handleFilterChange = (event) => {
-//     console.log(event.target.value)
-//     setFilter(event.target.value)
-//   }
-
-//   return (
-//     <div>
-//       <h2>Phonebook</h2>
-//       <Notification message={message} isError={isError}/>
-//       <Filter value={filter} onChange={handleFilterChange}/>
-//       <h2>
-//         add a new
-//       </h2>
-      
-//       <PersonForm onSubmit={addPerson} name={newName} nameHandle={handlePersonChange}
-//       num={newNum} numHandle={handleNumberChange}/>
-//       <h2>Numbers</h2>
-//       <Persons persons={persons} filter={filter} deleteHandler={deletePerson}/>
-//     </div>
-//   )
-// }
-
-// export default App
-
